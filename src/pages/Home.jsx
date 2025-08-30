70db26eb6f117e2b551e1b2f00117a2a8e0c87c7
An <trchan@students.cs.ubc.ca>
2025-08-30 13:09:55 -0700

    test blob

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ export const Home = () => {
         <ThemeToggle />
         
         {/* Background Effects */}
-        <Blob top="10%" left="20%" rgba1="rgba(63, 206, 250, 0.4)" rgba2="rgba(1, 95, 176, 0.5)" />
+        <Blob top="10%" left="20%" rgba1="rgba(63, 206, 250, 0.4)" rgba2="rgba(51, 176, 1, 0.6)" />
         <Blob top="50%" left="70%" rgba1="rgba(180, 82, 255, 0.4)" rgba2="rgba(30, 45, 255, 0.5)" />
         <Blob top="80%" left = "45%" rgba1="rgba(255, 128, 209, 0.4)" rgba2="rgba(255, 23, 170, 0.5)" />
         

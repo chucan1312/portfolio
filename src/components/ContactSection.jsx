3dae13fad31e0184d10b6d108db6c8c8b4fb6560
trchan <chucan1312@gmail.com>
2026-05-20 16:05:29 +0700

    Update linkedin hrefhref

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -54,7 +54,7 @@ export const ContactSection = () => {
                 <div className="pt-8">
                     <h4 className="font-medium mb-4">Connect With Me</h4>
                     <div className="flex space-x-4 justify-center">
-                        <a href="https://www.linkedin.com/in/chuc-an-trinh-891462332/" target="_blank">
+                        <a href="https://www.linkedin.com/in/an-trinh-891462332/" target="_blank">
                             <Linkedin />
                         </a>
                         <a href="https://www.instagram.com/trch.an/" target="_blank">
